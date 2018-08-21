1ee7e990e67d2d781635a659c6f223ea3ffa8950
Mervine, Jonathan <jmervine@eab.com>
2018-08-21 15:26:26 -0400

    Initial commit

---
+++ src/main/java/com/darkmage530/nowplayingchecker/NowplayingcheckerApplication.java
@@ -0,0 +1,12 @@
+package com.darkmage530.nowplayingchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowplayingcheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NowplayingcheckerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/darkmage530/nowplayingchecker/NowplayingcheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.darkmage530.nowplayingchecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NowplayingcheckerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
